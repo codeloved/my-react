b1049a6b414d437921f807e94aee8f8fe099f703
郑城城 <zhengcc@xbzoom.com>
2019-06-06 17:22:10 +0800

    添加bable

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+// import './index.css'
+import App from './pages/app/app.js'
+
+ReactDOM.render(<App />, document.getElementById('root'))
---
+++ src/pages/app/app.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+// import './app.css'
+
+class App extends Component {
+  clickApp = () => {
+    console.log('click app')
+  }
+  render() {
+    return(
+      <div className='app'>app pages</div>
+    )
+  }
+}
+
+export default App
